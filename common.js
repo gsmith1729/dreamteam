1c6c50e8efc47c7c8cd5f28d2691c92c9098df06
gsmith1729 <georgesmith1729@gmail.com>
2018-01-27 14:38:22 +0000

    timer

--- common.js
+++ common.js
@@ -1,12 +1,23 @@
+var timeslept=0
+var sleeping
+function sleep(){
+	timeslept+=1
+	document.getElementById('time').innerHTML=timeslept
+}
 document.getElementById("normalbed").style.display='inline'
 function changebed(){
 	if (document.getElementById("normalbed").style.display=='inline') {
 		document.getElementById("normalbed").style.display='none';
 		document.getElementById("bluebed").style.display='inline';
+		sleeping=setInterval(sleep,1000)
 	}
 	else {
 		document.getElementById("normalbed").style.display='inline';
 		document.getElementById("bluebed").style.display='none';
+		
+		clearInterval(sleeping)
+		document.getElementById('time').innerHTML=0
+		timeslept=0
 	}
 
 }
